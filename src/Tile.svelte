bc5d5f030c976aa96d8eb78aa2cf7d9f7f96efd6
TicTacTaw <thibault.crosnier@gmail.com>
2020-03-28 16:22:01 +0100

    feat(tile): tile number colors

--- src/Tile.svelte
+++ src/Tile.svelte
@@ -23,14 +23,41 @@
     border-radius: 4px;
     text-align: center;
     line-height: 30px;
-    border: solid 1px white;
+    border: solid 0.5px white;
     font-weight: bold;
   }
   .tile.revealed {
-    background-color: white;
+    background-color: #f5f5f5;
+  }
+  .tile[data-value='X'] {
+    background-color: #eb3939;
+  }
+  .tile[data-value='1'] {
+    color: #055dd1;
+  }
+  .tile[data-value='2'] {
+    color: #16a139;
+  }
+  .tile[data-value='3'] {
+    color: #ff6528;
+  }
+  .tile[data-value='4'] {
+    color: #eb3939;
+  }
+  .tile[data-value='5'] {
+    color: #df22fa;
+  }
+  .tile[data-value='6'] {
+    color: #8700e0;
+  }
+  .tile[data-value='7'] {
+    color: #00cece;
+  }
+  .tile[data-value='8'] {
+    color: #865454;
   }
   .tile.flagged {
-    background-color: green;
+    background-color: #ffdd44;
   }
 </style>
 
