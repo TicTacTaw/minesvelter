7ff8b4d29b6a19abfb15a54fc06da0f24c1e46b7
TicTacTaw <thibault.crosnier@gmail.com>
2020-03-29 13:32:14 +0200

    style(global): better font

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import App from './App.svelte';
+import './assets/styles/global.css';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'TT'
-	}
+  target: document.body,
+  props: {
+    name: 'TT',
+  },
 });
 
 window.app = app;
 
-export default app; 
+export default app;
